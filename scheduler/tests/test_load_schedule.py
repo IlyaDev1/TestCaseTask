4a1c58a9674798cc7e61a82fcb4d1f8243482bf1
Ilya Konovalov <devkvln@yandex.ru>
2025-07-21 17:10:17 +0300

    added conftest and first test

--- scheduler/tests/test_load_schedule.py
+++ scheduler/tests/test_load_schedule.py
@@ -1,7 +1,9 @@
 from scheduler import Scheduler
 
+from .schedule_instances import correct_schedule_by_url
 
-def test_correct_load_by_url(
-    schedule_instance: Scheduler, correct_schedule_instance: dict
-):
-    assert schedule_instance.schedule == correct_schedule_instance
+
+def test_correct_load_by_url(schedule_instance: Scheduler):
+    """Тест проверяет, что c url данные загружаются корректно."""
+
+    assert schedule_instance.schedule == correct_schedule_by_url
