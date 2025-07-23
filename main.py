221b76b60695766f93eb4c900a04424cb2f735c9
Ilya Konovalov <devkvln@yandex.ru>
2025-07-23 16:34:34 +0300

    added code to main

---
+++ main.py
@@ -0,0 +1,19 @@
+from scheduler import Scheduler
+
+scheduler = Scheduler(url="https://ofc-test-01.tspb.su/test-task/")
+
+# Получение всех занятых промежутков для даты 2025-02-15
+print(scheduler.get_busy_slots("2025-02-15"))
+
+# Получение всех свободных промежутков для даты 2025-02-15
+print(scheduler.get_free_slots("2025-02-15"))
+
+# Проверка доступности временного промежутка
+print(scheduler.is_available("2025-02-15", "12:00", "13:00"))
+
+print(scheduler.is_available("2025-02-15", "11:30", "12:30"))
+
+# Поиск первого подходящего свободного времени для заявки на 60 минут и на 90 минут
+print(scheduler.find_slot_for_duration(duration_minutes=60))
+
+print(scheduler.find_slot_for_duration(duration_minutes=90))
